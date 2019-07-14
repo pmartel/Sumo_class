042460baf36a57bc323ba78f56a50d6a64f63949
Philip Martel <pmartel@alum.mit.edu>
2019-07-13 22:32:34 -0400

    Update Sumo_Class.ino
    Noah's latest code 7/13/19 19:47
    Replaced pin numbers with symbolic names

--- Sumo_Class.ino
+++ Sumo_Class.ino
@@ -1,27 +1,36 @@
 #include <NewPing.h>
-
+const int left_forward = 10;//while pwm is high moves left motor forward
+const int left_backward = 11;//while pwm is high moves left motor backward
+const int right_forward = 9;//while pwm is high moves right motor forward
+const int right_backward = 8;//while pwm is high moves right motor backward
+const int left_side = 13;//left edge sensor
+const int right_side = 12;//right edge sensor
 NewPing Sonar(5,6, 60);
 void setup() {
   // put your setup code here, to run once:
-  pinMode(10, OUTPUT);
-  pinMode(11, OUTPUT);
-  pinMode(9, OUTPUT);
-  pinMode(8, OUTPUT);
-  pinMode(13, INPUT);
-  pinMode(12, INPUT);
+  //Pin setup is declared here.
+  pinMode(left_forward, OUTPUT);
+  pinMode(left_backward, OUTPUT);
+  pinMode(right_forward, OUTPUT);
+  pinMode(right_backward, OUTPUT);
+  pinMode(left_side, INPUT);
+  pinMode(right_side, INPUT);
   Serial.begin(9600);
 }
-bool obstacle_mode = true;
-int duration = 50;
+bool obstacle_mode = true;// Obstacle mode is declared true by default until an edge is reached where it is inverted from there.
+int duration = 50;// Duration of the robots scan for other nearby robots is decalred here.
 void loop() {
  
-  int  dist;
+  int  dist;// Sonar.ping_cm() is this variables near equivilent.
   // put your main code here, to run repeatedly:
-  Serial.println(Sonar.ping_cm());
-  if (digitalRead(13) == HIGH && digitalRead(12) == HIGH && obstacle_mode == true){
+  Serial.println(Sonar.ping_cm());// Prints the robots sonar readings for debugging
+
+  //If the robot is in the near center of the arena and obstacle mode is true the robot moves forward.
+  if (digitalRead(left_side) == HIGH && digitalRead(right_side) == HIGH && obstacle_mode == true){
     forward(255);
   }
-  if (digitalRead(13) == LOW || digitalRead(12) == LOW){
+  //If the robot is on an edge of the arena obstacle mode is inverted, the robot moves backwards, and then turns left.
+  if (digitalRead(left_side) == LOW || digitalRead(right_side) == LOW){
     backward(200);
     delay(1000);
     Left(255);
@@ -33,40 +42,44 @@ void loop() {
       obstacle_mode = false; 
     }
   }
+  //if obstacle mode is false robot scans the arena for another bot. The duration of the scan is defined above the loop. 
   if (obstacle_mode == false){
-    Left(125); 
+    Left(255); 
+    delay(15);
+    Stop();
+    delay(30);
     duration -= 1;
     dist= Sonar.ping_cm();
     Serial.println(dist);
-    if (dist <= 30 && dist != 0 || duration == 0){
+    if (dist <= 30 && dist != 0 || duration <= 0){//if the scan turns up something obstacle mode becomes true and the robot will run over the other bot. 
       duration = 50;
       obstacle_mode = true;
     }
   }
 }
-void backward(int Speed){
-  analogWrite(11, Speed);
-  analogWrite(10, 0);
-  analogWrite(9, Speed);
-  analogWrite(8, 0);
+void backward(int Speed){//backwards code is defined here.
+  analogWrite(left_backward, Speed);
+  analogWrite(left_forward, 0);
+  analogWrite(right_forward, Speed);
+  analogWrite(right_backward, 0);
 }
-void forward(int Speed){
-  analogWrite(11, 0);
-  analogWrite(10, Speed);
-  analogWrite(9, 0);
-  analogWrite(8, Speed);
+void forward(int Speed){//forward code is defined here.
+  analogWrite(left_backward, 0);
+  analogWrite(left_forward, Speed);
+  analogWrite(right_forward, 0);
+  analogWrite(right_backward, Speed);
 }
-void Left(int Speed){
-  analogWrite(11, Speed);
-  analogWrite(10, 0);
-  analogWrite(9, 0);
-  analogWrite(8, Speed);
+void Left(int Speed){//Left code is defined here.
+  analogWrite(left_backward, Speed);
+  analogWrite(left_forward, 0);
+  analogWrite(right_forward, 0);
+  analogWrite(right_backward, Speed);
 }
-void Stop()
+void Stop()//robot stop code is defined here if need be
 {
-  analogWrite(11, 0);
-  analogWrite(10, 0);
-  analogWrite(9, 0);
-  analogWrite(8, 0);
+  analogWrite(left_backward, 0);
+  analogWrite(left_forward, 0);
+  analogWrite(right_forward, 0);
+  analogWrite(right_backward, 0);
 }
 
